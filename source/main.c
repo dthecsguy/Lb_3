7cd7465bf42894d76b03c41eb3adf472757bfbe1
Demarcus Sales <dsale010@wch136-13.cs.ucr.edu>
2020-08-11 09:05:27 -0700

    ok

--- source/main.c
+++ source/main.c
@@ -13,10 +13,26 @@
 #endif
 
 int main(void) {
-    /* Insert DDR and PORT initializations */
+    DDRA = 0x00; PORTA = 0xFF;
+    DDRB = 0x00; PORTB = 0xFF;
+    DDRC = 0xFF; PORTC = 0x00;
+    
+    unsigned char loop = 8, tmpA = PINA, tmpB = PINB, cnt = 3;
 
     /* Insert your solution below */
     while (1) {
+        while (loop >= 0){
+            if (tmpA & 0x01)
+                cnt++;
+            
+            if (tmpB & 0x01)
+                cnt++;
+            
+            tmpA = tmpA >> 1;
+            tmpB = tmpB >> 1;
+            loop--;
+        }
+        PORTC = cnt;
 
     }
     return 1;
