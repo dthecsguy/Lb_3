dfa61421cb8c528f8449e7fa7fbe83943a6f4119
Demarcus Sales <dsale010@wch136-13.cs.ucr.edu>
2020-08-12 08:52:34 -0700

    Completed Part 4

---
+++ turnin/dsale010_lab3_part4.c
@@ -0,0 +1,30 @@
+/*	Author: dsale010
+ *  Partner(s) Name: 
+ *	Lab Section:
+ *	Assignment: Lab #  Exercise #
+ *	Exercise Description: [optional - include for your own benefit]
+ *
+ *	I acknowledge all content contained herein, excluding template or example
+ *	code, is my own original work.
+ */
+#include <avr/io.h>
+#ifdef _SIMULATE_
+#include "simAVRHeader.h"
+#endif
+
+#define A PINA
+#define Ahigh (PINA & 0xF0)
+#define Alow (PINA & 0x0F)
+
+int main(void) {
+    DDRA = 0x00; PORTA = 0xFF;
+    DDRB = 0xFF; PORTB = 0x00;
+    DDRC = 0xFF; PORTC = 0x00;
+
+    /* Insert your solution below */
+    while (1) {
+        PORTB = (Ahigh >> 4) & 0x0F;
+        PORTC = (Alow << 4) & 0xF0;
+    }
+    return 1;
+}
